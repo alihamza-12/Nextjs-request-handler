af6245024fffdc49f387ea866ec085e50e3659e8
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 14:13:47 +0500

    Update in the GET _request=not using that value

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -1,6 +1,6 @@
 import { comments } from "../data";
 //Dynamic Route
-export async function GET(request : Request , { params }: { params: Promise<{ id: string }> } ){
+export async function GET(_request : Request , { params }: { params: Promise<{ id: string }> } ){
     const {id} =await  params
 
     const comment = comments.find((comment)=>comment?.id=== parseInt(id));
