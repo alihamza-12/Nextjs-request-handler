bb2ad601df8e1d2ddade56412d2cead965495aaa
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 12:41:25 +0500

    Post request and update data.ts according to ts

--- app/api/comments/data.ts
+++ app/api/comments/data.ts
@@ -1,4 +1,9 @@
-export const comments = [
+export const comments: {
+  id: number;
+  user: string;
+  comment: string;
+  createdAt: string | Date;
+}[] = [
   {
     id: 1,
     user: "Ali",
@@ -10,17 +15,5 @@ export const comments = [
     user: "Ahmed",
     comment: "Very helpful article.",
     createdAt: "2026-06-22T10:05:00Z",
-  },
-  {
-    id: 3,
-    user: "Sara",
-    comment: "Thanks for sharing.",
-    createdAt: "2026-06-22T10:10:00Z",
-  },
-  {
-    id: 4,
-    user: "Fatima",
-    comment: "I learned something new today.",
-    createdAt: "2026-06-22T10:15:00Z",
   },
 ];
--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -3,3 +3,28 @@ import { comments } from "./data";
 export async function GET() {
     return Response.json(comments)
 }
+
+export async function POST(request:Request){
+    const {user ,comment} =await request.json();
+
+    if (!user || !comment) {
+    return Response.json(
+      { error: "user and comment are required" },
+      { status: 400 }
+    );
+  }
+
+    const newComment={
+        id:comments.length + 1,
+        user ,
+        comment ,
+        createdAt : new Date(),
+    }
+    
+    comments.push(newComment);
+
+    return Response.json(newComment,{
+        // headers:{'Content-Type':'application/json'},
+        status:201,
+    })
+}
