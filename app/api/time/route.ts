076b3ac19ddb4924ad44f33f9578cc10beaaddd5
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 18:34:31 +0500

    cashing and not cashing not working

---
+++ app/api/time/route.ts
@@ -0,0 +1,16 @@
+// No caching (always fresh)
+// export const dynamic = "force-dynamic";
+// -----------------------------------------
+// Cache response for 60 seconds
+// Same response served to all users
+export const revalidate = 50;
+
+export async function GET(request:Request) {
+    const time =new Date().toLocaleTimeString();
+    console.log(time)
+
+     return Response.json({
+    message: "Fetched time",
+    time
+    });
+}
