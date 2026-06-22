75614c12bb81664b1d745629a27311ee156cbbdf
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 14:38:50 +0500

    PATCH request handler for updating the comment

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -5,18 +5,36 @@ export async function GET(_request : Request , { params }: { params: Promise<{ i
 
     const comment = comments.find((comment)=>comment?.id=== parseInt(id));
     if (!comment) {
-    return Response.json(
-        { message: "Comment not found"},
-        { status: 404 }
-    )    
-  }
-
-  
+        return Response.json(
+            { message: "Comment not found"},
+            { status: 400 }
+         )    
+    }
     return Response.json({
     message: "Fetched comment",
     comment,
     });
-  
+}
+
+//PATCH request
+export async function PATCH(request: Request,{params}:{params : Promise<{id:string}>}){
+    const {id}=await params;
+    const body= await request.json()
+    const {comment}=await body
+
+    if (!comment) {
+     return Response.json(
+       { message: "Comment not found"},
+       { status: 404 }
+     )    
+   }
+
+    const index= comments.findIndex((comment)=>comment.id===parseInt(id));
+    comments[index].comment=comment;
 
 
+    return Response.json({
+    message: "Fetched comment",
+    comment:comments[index]
+    });
 }
