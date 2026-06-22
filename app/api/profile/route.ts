065a07e709e221bd3b98e30e15ca770debd92d52
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 17:21:33 +0500

    Set cookie retrive cookie using header and cookie

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(request: Request) {
   // Option A: From Request
@@ -14,10 +14,33 @@ export async function GET(request: Request) {
 
   console.log(token);
 
+  //1-Retrive Cookie 
+  const getCookie = request.headers.get("cookie");
+  console.log(getCookie)
+
+  //2-Retrive cookie
+  const cookieStore= await cookies();
+  //Retrive
+  console.log(cookieStore.get('token')?.value)
+  console.log(cookieStore.get('theme')?.value)
+  //Set cookie
+  cookieStore.set('theme','dark')
+  //for delete 
+  // cookieStore.delete('theme')
+
+
   return Response.json({
     message: "Fetched Headers",
     userAgent,
     authorization,
     token,
-  });
+  },
+  //Set the new header
+  {
+    headers: {
+    Authorization: "Bearer 1324",
+    "Set-Cookie": "token=11223344; Path=/; HttpOnly; SameSite=Strict",
+    }
+  }
+ );
 }
