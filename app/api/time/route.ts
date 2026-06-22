19de97ce3f0414f5a80dfdd1628c89ad711f38b5
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 18:36:45 +0500

    dynamic="force-dynamic"no cashing run on every req

--- app/api/time/route.ts
+++ app/api/time/route.ts
@@ -3,7 +3,7 @@
 // -----------------------------------------
 // Cache response for 60 seconds
 // Same response served to all users
-export const revalidate = 50;
+// export const revalidate = 50;
 
 export async function GET(request:Request) {
     const time =new Date().toLocaleTimeString();
