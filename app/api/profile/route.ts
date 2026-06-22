e7c1d3766a8ce69363f2938bdf837034492d8288
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-22 16:40:57 +0500

    Headers in route Handlers

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,5 +1,23 @@
-export async function GET(request:Request){
-    return Response.json({
-    message: "All users",
+import { headers } from "next/headers";
+
+export async function GET(request: Request) {
+  // Option A: From Request
+  const userAgent = request.headers.get("user-agent");
+  const authorization = request.headers.get("authorization");
+
+  console.log(userAgent);
+  console.log(authorization);
+
+  // Option B: From next/headers
+  const headersList = await headers();
+  const token = headersList.get("authorization");
+
+  console.log(token);
+
+  return Response.json({
+    message: "Fetched Headers",
+    userAgent,
+    authorization,
+    token,
   });
 }
