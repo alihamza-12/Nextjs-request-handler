345751739dd027dd2edeb073e3fc51e6e9d9033c
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-23 11:46:18 +0500

    Reading and sending cookie for set

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,23 @@
 import { redirect } from "next/dist/server/api-utils";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export  function middleware(request:NextRequest){
+export async function middleware(request:NextRequest){
     //Response send back to server
     const response= NextResponse.next()
-    console.log("👉 MIDDLEWARE IS RUNNING ON PATH:", request.nextUrl.pathname);
+    // console.log("👉 MIDDLEWARE IS RUNNING ON PATH:", request.nextUrl.pathname);
     // return NextResponse.redirect(new URL('/',request.url))
     if(request.nextUrl.pathname==='/api/profile'){
        return NextResponse.redirect(new URL('/',request.url))
     }
+    //Getting Cookie and set again
+     //2-Retrive cookie
+    const cookieStore= await cookies();
+    //Retrive
+    console.log(cookieStore.get('token')?.value)
+    console.log(cookieStore.get('theme')?.value)
+    //Set cookie
+    cookieStore.set('theme','light')
     //Set headers for adition information
     response.headers.set('custome-header' , 'custom-value')
     return response
