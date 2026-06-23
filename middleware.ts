c3fc19af855ec3c5dded0c167a0764798cdf04bd
alihamza-12 <alihamzaarshad12@gmail.com>
2026-06-23 11:42:35 +0500

    Set header in middleware in response

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,16 @@ import { redirect } from "next/dist/server/api-utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export  function middleware(request:NextRequest){
-
+    //Response send back to server
+    const response= NextResponse.next()
     console.log("👉 MIDDLEWARE IS RUNNING ON PATH:", request.nextUrl.pathname);
     // return NextResponse.redirect(new URL('/',request.url))
     if(request.nextUrl.pathname==='/api/profile'){
        return NextResponse.redirect(new URL('/',request.url))
     }
+    //Set headers for adition information
+    response.headers.set('custome-header' , 'custom-value')
+    return response
 }
 
 // export const config = {
